67f4997d058c764d3100ddeb12855e4d358a7202
LutsenkoVitaliy <lutsenko996@gmail.com>
2022-10-17 23:50:56 +0300

    sectionsContainer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
-export const App = () => {
-  return (
-    <>
-    </>
-  );
-};
+import React, { Component } from 'react';
+import Section from "./Section/Section";
+
+class App extends Component {
+  render() {
+    return (
+  <>
+  <Section title="Please leave feedback">
+    {/* <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions> */}
+  </Section>
+  <Section title="Statistics">
+    {/* <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics> */}
+  </Section>
+  </>      
+    )}
+}
+
+export default App;
